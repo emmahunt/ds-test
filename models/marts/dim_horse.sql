260ee33ec3212b0820e81aa9294aad9fdb8f7e39
Emma Hunt <emmaghunt95@gmail.com>
2023-02-14 18:58:57 +0000

    Convert dim_horse to slowly changing dimension, where changes to the trainer are captured

--- models/marts/dim_horse.sql
+++ models/marts/dim_horse.sql
@@ -1,15 +1,41 @@
-select
-    horse_id
-    -- , weight_value
-    -- , trainer_id
-    -- , last_run_days_flat
-    , gender
-    , case
-        when gender in ('colt', 'stallion', 'gelding') then 'male'
-        when gender in ('filly', 'mare', 'broodmare', 'dam') then 'female'
-    end as sex
-    , colour
-    , year_born
-    , count_if(won) as number_of_races_won
-from {{ ref('stg_horses') }}
-group by 1, 2, 3, 4, 5
+
+with horses as (
+    select *
+        , min(meeting_date) over (partition by horse_id order by meeting_date asc) as first_meeting_date
+        , lag(trainer_id) over (partition by horse_id order by meeting_date asc) as previous_trainer_id
+    from {{ ref('stg_horses') }}
+)
+
+, identify_trainer_change as (
+    select distinct
+        horse_id
+        , gender
+        , colour
+        , year_born
+        , first_meeting_date
+        , trainer_id
+        , meeting_date
+    from horses
+    where 
+        meeting_date = first_meeting_date 
+            or trainer_id != previous_trainer_id
+)
+
+, final as (
+    select
+        horse_id
+        , gender
+        , case
+            when gender in ('colt', 'stallion', 'gelding') then 'male'
+            when gender in ('filly', 'mare', 'broodmare', 'dam') then 'female'
+        end as sex
+        , colour
+        , year_born
+        , first_meeting_date
+        , trainer_id
+        , meeting_date as valid_from
+        , lead(meeting_date) over (partition by horse_id order by meeting_date asc) as valid_to
+    from identify_trainer_change
+)
+
+select * from final
