55d50fb28d02788dda10e9666cbf605795fbcde7
Emma Hunt <emmaghunt95@gmail.com>
2023-02-16 17:40:03 +0000

    Adjust dim_race to include winning horses

--- models/marts/dim_race.sql
+++ models/marts/dim_race.sql
@@ -1,8 +1,25 @@
-select distinct
-    race_id
-    , horse_count
-    , course_distance_metres
-    , meeting_date
-    , meeting_id
-from {{ ref('stg_horses') }}
 
+with stg_horses as (select * from {{ ref('stg_horses') }})
+
+, winners as (
+    select race_id
+        , array_agg(distinct horse_id) as winning_horse_ids
+    from stg_horses
+    where won
+    group by 1
+)
+
+, final as (
+    select distinct
+        stg_horses.race_id
+        , winners.winning_horse_ids
+        , stg_horses.horse_count
+        , stg_horses.course_distance_metres
+        , stg_horses.meeting_date
+        , stg_horses.meeting_id
+    from stg_horses
+    left join winners
+        on stg_horses.race_id = winners.race_id
+)
+
+select * from final
